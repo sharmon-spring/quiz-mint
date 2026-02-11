0ef1aa05fea467123d7323a7d809cb3be7b1c47f
sharmon-spring <245662567+sharmon-spring@users.noreply.github.com>
2026-02-11 20:44:09 +0900

    仮の問題の追加

---
+++ script.js
@@ -0,0 +1,12 @@
+const questions = [
+  {
+    question: "問題１",
+    answers: ["あ", "い", "う", "え"],
+    correct: 0
+  },
+  {
+    question: "問題２",
+    answers: ["1", "2", "3", "4"],
+    correct: 2
+  }
+];
